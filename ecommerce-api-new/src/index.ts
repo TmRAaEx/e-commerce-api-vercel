8844e23cf1aad7cea4344000db328a74b1cd1e28
TmRAaEx <tmr.aaex@gmail.com>
2025-04-03 10:58:43 +0200

    dsd

--- ecommerce-api-new/src/index.ts
+++ ecommerce-api-new/src/index.ts
@@ -1,21 +1,24 @@
 import express from "express";
-import {connectDB} from "./config/db";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import { connectDB } from "./config/db";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import cron from "node-cron";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  // origin: "http://localhost:5173",
-  origin: "*",
-  credentials: true,  // ✅ Allows cookies
-}));
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+    origin: "*",
+    credentials: true, // ✅ Allows cookies
+  })
+);
 
 // Routes
 import productRouter from "./routes/products";
@@ -24,22 +27,62 @@ import orderRouter from "./routes/orders";
 import orderItemRouter from "./routes/orderItems";
 import stripeRouter from "./routes/checkout";
 import authRouter from "./routes/customers";
-import adminRouter from "./routes/admins"
-app.use('/products', productRouter)
-app.use('/customers', customerRouter)
-app.use('/orders', orderRouter)
-app.use('/order-items', orderItemRouter)
-app.use('/checkout', stripeRouter)
-app.use('/auth', authRouter)
-app.use('/admin', adminRouter)
+import adminRouter from "./routes/admins";
+import moment from "moment";
+import { deleteOrder, getOrders } from "./services/orderService";
+app.use("/products", productRouter);
+app.use("/customers", customerRouter);
+app.use("/orders", orderRouter);
+app.use("/order-items", orderItemRouter);
+app.use("/checkout", stripeRouter);
+app.use("/auth", authRouter);
+app.use("/admin", adminRouter);
 
 // Attempt to connect to the database
-connectDB()
+connectDB();
+
+
+// remove unhandled orders
+cron.schedule("0 * * * *", async () => {
+  console.log("Cron job running every hour");
+
+  try {
+    const orders = await getOrders();
+    const currentTime = moment();
+
+    // Find orders that are over an hour old and have a "pending" status
+    const outdatedOrderIds = orders
+      .filter((order) => {
+        const orderCreatedAt = moment(order.created_at);
+        return (
+          currentTime.diff(orderCreatedAt, "hours") > 1 &&
+          order.order_status === "pending"
+        );
+      })
+      .map((order) => order.id);
+    console.log(
+      "Outdated order IDs (created more than an hour ago):",
+      outdatedOrderIds
+    );
+
+    // Use Promise.all to delete all outdated orders
+    if (outdatedOrderIds.length > 0) {
+      await Promise.all(outdatedOrderIds.map((id) => deleteOrder(id)));
+      console.log(
+        `Successfully deleted ${outdatedOrderIds.length} outdated orders.`
+      );
+    } else {
+      console.log("No outdated orders to delete.");
+    }
+  } catch (error) {
+    console.error("Error running cron job:", error);
+  }
+});
 // Start Express server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
-})
+});
 
 export default (req: VercelRequest, res: VercelResponse) => {
   return app(req, res);
